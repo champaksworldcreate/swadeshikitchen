6d2d342c104f64f14b297c462a18bbd26594ae36
champaksworldcreate <champaksworld@gmail.com>
2024-01-04 16:19:53 +0530

    pw converted error removed

--- sw.js
+++ sw.js
@@ -1,6 +1,6 @@
 var GHPATH = '/swadeshikitchen';
 var APP_PREFIX = 'shubham_';
-var VERSION = 'version_002';
+var VERSION = 'version_003';
 var URLS = [    
   `${GHPATH}/`,
   `${GHPATH}/index.html`,

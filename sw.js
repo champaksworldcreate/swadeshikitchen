c6068a744a0ab61c10674e461f9659b720ec45ef
champaksworldcreate <champaksworld@gmail.com>
2024-01-04 16:12:31 +0530

    error2 versioning

--- sw.js
+++ sw.js
@@ -1,6 +1,6 @@
 var GHPATH = '/swadeshikitchen';
 var APP_PREFIX = 'shubham_';
-var VERSION = 'version_001';
+var VERSION = 'version_002';
 var URLS = [    
   `${GHPATH}/`,
   `${GHPATH}/index.html`,
